746a3866bc3fd2e5abbfde36cf02ad07ab2568b5
Colin Schaefer <colin.schaefer@gmx.de>
2015-06-19 11:46:31 -0400

    Bug fix for the spinners.

--- src/main/java/bdv/tools/zdim/ZdimDialog.java
+++ src/main/java/bdv/tools/zdim/ZdimDialog.java
@@ -10,6 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.WindowConstants;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -37,6 +39,10 @@ public class ZdimDialog extends JDialog {
 			final SetupAssignments setupAssignments, final int width) {
 		super(owner, "Z - dimension of max-projection", false);
 
+		SpinnerModel modelin = new SpinnerNumberModel(value, 0, 2000, 1);
+		SpinnerModel modelmin = new SpinnerNumberModel(min, 0, 2000, 1);
+		SpinnerModel modelmax = new SpinnerNumberModel(max, 0, 2000, 1);
+
 		// setup the dialog
 		setSize(500, 120);
 		setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
@@ -56,13 +62,9 @@ public class ZdimDialog extends JDialog {
 
 		// creating the spinners, setting them right aligned thext and setting
 		// the default value
-		final JSpinner inputField = new JSpinner();
-		final JSpinner maxField = new JSpinner();
-		final JSpinner minField = new JSpinner();
-
-		inputField.setValue(value);
-		minField.setValue(min);
-		maxField.setValue(max);
+		final JSpinner inputField = new JSpinner(modelin);
+		final JSpinner maxField = new JSpinner(modelmax);
+		final JSpinner minField = new JSpinner(modelmin);
 
 		// adding the textfields and labels to the panel
 		rightPanel.add(minField);
@@ -88,29 +90,17 @@ public class ZdimDialog extends JDialog {
 
 		// listener for the input field, that changes the slider accordingly and
 		// adjusts the minimum and maximum value if needed
+
 		inputField.addChangeListener(new ChangeListener() {
+
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				value = Integer.valueOf(inputField.getValue().toString());
-				int tmpValue = value;
-				if (tmpValue < min && tmpValue >= 20) {
-					min = tmpValue - 20;
-				} else if (tmpValue < min && tmpValue < 20) {
-					min = 0;
-				} else if (tmpValue > max) {
-					max = tmpValue + 20;
-				}
+				value = (Integer) inputField.getValue();
+				System.out.println("value" + String.valueOf(value));
 
-				microns.setMinimum(min);
-				microns.setMaximum(max);
-				microns.setLabelTable(null);
-				microns.setMajorTickSpacing((max - min) / 10);
-				minField.setValue(String.valueOf(min));
-				maxField.setValue(String.valueOf(max));
+				microns.setValue(value);
 
-				microns.setValue(tmpValue);
-
-				value = tmpValue;
+				System.out.println(String.valueOf(value));
 			}
 		});
 
@@ -119,18 +109,9 @@ public class ZdimDialog extends JDialog {
 		minField.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				min = Integer.valueOf(minField.getValue().toString());
-				if (min < 0) {
-					min = 0;
-					minField.setValue(String.valueOf(min));
-				}
-
-				if (min >= max) {
-					max = min + 50;
-					maxField.setValue(String.valueOf(max));
-				}
+				min = (Integer) minField.getValue();
+				System.out.println("min" + String.valueOf(min));
 				microns.setMinimum(min);
-				microns.setMaximum(max);
 				microns.setLabelTable(null);
 				microns.setMajorTickSpacing((max - min) / 10);
 			}
@@ -142,27 +123,13 @@ public class ZdimDialog extends JDialog {
 		maxField.addChangeListener(new ChangeListener() {
 			@Override
 			public void stateChanged(ChangeEvent e) {
-				max = Integer.valueOf(maxField.getValue().toString());
-				if (max < 0) {
-					max = 100;
-					min = 0;
-					minField.setValue(String.valueOf(min));
-					maxField.setValue(String.valueOf(max));
-					microns.setValue(20);
-				}
-
-				if (max < min && max > 50) {
-					min = max - 50;
-					minField.setValue(String.valueOf(min));
-				} else {
-					min = 0;
-					minField.setValue(String.valueOf(min));
-				}
-
-				microns.setMinimum(min);
+				max = (Integer) maxField.getValue();
 				microns.setMaximum(max);
 				microns.setLabelTable(null);
 				microns.setMajorTickSpacing((max - min) / 10);
+
+				System.out.println("max" + String.valueOf(max));
+
 			}
 		});
 
